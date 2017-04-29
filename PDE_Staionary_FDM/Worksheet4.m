b2360c242d1f7960fbb276a7ecb20144a91d6fa1
Abhijeet Parida <abhijeet.parida@tum.de>
2017-04-29 12:04:58 +0200

    Worksheet 4: Finite Diff Method for stationary PDE

---
+++ PDE_Staionary_FDM/Worksheet4.m
@@ -0,0 +1,60 @@
+clc;
+clear
+
+% Boundary of the PDE 
+x_l=0;
+x_r= 1;
+
+y_d=0;
+y_u= 1;
+
+global var;
+var=1;
+
+N =[7,15,31,63,127]; % J: number of division for x
+error=zeros(1,size(N,2));
+
+for i=1:size(N,2)
+Nx=N(i);
+Ny = Nx;
+
+dx = (x_r-x_l) / (Nx+1);
+dy = dx;
+
+%generate grid points
+x=x_l:dx:x_r;
+y=x;
+
+[U,T]=discreditisation(Nx,Ny,dx);
+
+%MATLAB direct solver
+if (Nx<100)
+fprintf('\nMATLAB Full Matrix Solver Nx=%.0f\n',Nx)
+sol_direct(U,T,Nx,1,x,y);
+end
+
+%MATLAB direct solver sparse
+if (Nx<100)
+fprintf('\nMATLAB Sparse Matrix Solver Nx=%.0f\n',Nx)
+sol_direct(sparse(U),T,Nx,2,x,y);
+end
+
+%Gauss Siedel solver 
+fprintf('\nMATLAB Gauss Seidel Matrix Solver Nx=%.0f\n',Nx)
+ans1=sol_gauss_siedel(U,T,Nx,x,y);
+
+%analytical solution
+analytical=zeros(Nx+2,Nx+2);
+for a=1:Nx+1
+analytical(a,1:Nx+1)=sin(pi*(a-1)/(Nx+1))*sin(pi*(0:Nx)./(Nx+1));
+end
+
+%error analysis Gauss Seidel
+error(i)=sqrt((sum(sum((ans1-analytical).^2),2))/(Nx*Ny));
+end
+error_reduction(2:size(error,2))=error(1:size(error,2)-1)./error(2:size(error,2));
+
+%print gauss table
+fprintf('\n\n')
+disp('      GAUSS SEIDEL ERROR ANALYSIS')
+disp(table(N', error', error_reduction','VariableNames',{'Division' 'error' 'error_red'}))
---
+++ PDE_Staionary_FDM/sol_gauss_siedel.m
@@ -0,0 +1,53 @@
+function [ans1] = sol_gauss_siedel(U,T,Nx,x,y)
+%SOL_GAUSS_SIEDEL Gauss Seidel Solver
+%   The solver to solve the system of linear equation using the iterativ
+%   Gauss Seidel Solver
+
+residual =1;
+
+%init for prev and cuurent iterartions
+midway= zeros(Nx^2,1) ;
+midway_1 = zeros(Nx^2,1);
+
+ans1= zeros(Nx+2,Nx+2);
+
+tic;
+while residual > 1*10^-4
+    for i=1:Nx^2
+        
+         sum1 = 0;        
+         sum2 = 0;
+         
+        if(i-1>0 && mod((i-1),Nx)~=0)
+            sum1 = sum1 + midway_1(i-1);
+        end
+        if(i+1<=Nx*Nx && mod((i),Nx)~=0)
+            sum2=sum2+midway(i+1);
+        end
+       if(i+Nx<=Nx*Nx)
+            sum2=sum2+midway(i+Nx);
+        end
+        if(i-Nx>0)
+             sum1 = sum1 + midway_1(i-Nx);
+        end
+         midway_1(i) = (T(1,i) - sum1 - sum2)/(-4);
+        end
+    
+     residual=sqrt(sum((sum(T'-U*midway_1).^2),2));
+   
+     midway = midway_1;
+
+end
+toc;
+
+for i=0:Nx-1
+    ans1(2:Nx+1,i+2)=midway(i*Nx+1:(i+1)*Nx,1);
+end
+
+plotter(3,x,y,ans1,Nx);
+
+fprintf('Total matrix/vector entries=%.0f\n',size(ans1,2)*size(ans1,1)+size(T,2))
+end
+
+
+
